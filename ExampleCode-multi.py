d5be75c6c4651b6f887e6c27467692b7bcbcc625
Kelly McDonald <kelly@fammcdonald.net>
2020-09-23 10:00:34 -0400

    Fix up BPMN document which had been committed without a proper merge - still had conflict messages in it. Backported to Camunda 3.7 so that it would be compatible with spiff. Next step is to add the ability to use the jinja documentation portion.

---
+++ ExampleCode-multi.py
@@ -0,0 +1,41 @@
+from SpiffWorkflow.bpmn.workflow import BpmnWorkflow
+from SpiffWorkflow.camunda.parser.CamundaParser import CamundaParser
+from SpiffWorkflow.camunda.specs.UserTask import EnumFormField, UserTask
+
+
+def show_form(task):
+    model = {}
+    form = task.task_spec.form
+
+    if task.data is None:
+        task.data = {}
+
+    for field in form.fields:
+        prompt = field.label
+        if isinstance(field, EnumFormField):
+            prompt += "? (Options: " + ', '.join([str(option.id) for option in field.options]) + ")"
+        prompt += "? "
+        answer = input(prompt)
+        if field.type == "long":
+            answer = int(answer)
+        task.update_data_var(field.id,answer)
+
+x = CamundaParser()
+x.add_bpmn_file('multi_instance_array.bpmn')
+spec = x.get_spec('MultiInstanceArray')
+
+workflow = BpmnWorkflow(spec)
+
+workflow.do_engine_steps()
+ready_tasks = workflow.get_ready_user_tasks()
+while len(ready_tasks) > 0:
+    for task in ready_tasks:
+        if isinstance(task.task_spec, UserTask):
+           show_form(task)
+           print(task.data)
+        else:
+            print("Complete Task ", task.task_spec.name)
+        workflow.complete_task_from_id(task.id)
+    workflow.do_engine_steps()
+    ready_tasks = workflow.get_ready_user_tasks()
+print(workflow.last_task.data)
