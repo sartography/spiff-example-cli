e989682ce56b8fe1c25ed5902e4974052e1612ee
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2024-07-03 12:36:40 -0400

    add migration example

---
+++ spiff_example/cli/diff_result.py
@@ -0,0 +1,67 @@
+import sys
+
+from SpiffWorkflow import TaskState
+
+def get_name_and_start(spec_diff):
+    for ts in spec_diff.alignment:
+        if ts == ts._wf_spec.start:
+            return ts._wf_spec.name, ts
+
+def show_spec_diff(spec_diff):
+
+    name, start = get_name_and_start(spec_diff)
+    updates, checked = [], []
+
+    # This determines what needs to be be displayed (omits specs with exact mappings)
+    def order_specs(ts):
+        for output in [s for s in ts.outputs if s not in checked]:
+            checked.append(output)
+            if output in spec_diff.changed or spec_diff.alignment[output] is None:
+                updates.append(output)
+            order_specs(output)
+
+    order_specs(start)
+
+    sys.stdout.write(f'\nResults for {name}\n\n')
+    sys.stdout.write('Changed or Removed\n')
+
+    for ts in updates:
+        # Ignore tasks that don't appear in diagrams
+        detail = ''
+        if ts.bpmn_id is not None:
+            if spec_diff.alignment[ts] is None:
+                detail = 'removed'
+            elif ts in spec_diff.changed:
+                detail = ', '.join(spec_diff.changed[ts])
+        if detail:
+            label = f'{ts.bpmn_name or '-'} [{ts.name}]'
+            sys.stdout.write(f'{label:<50s} {detail}\n')
+
+    if len(spec_diff.added) > 0:
+        sys.stdout.write('\nAdded\n')
+        for ts in spec_diff.added:
+            if ts.bpmn_id is not None:
+                sys.stdout.write(f'{ts.bpmn_name or '-'} [{ts.name}]\n')
+
+
+def show_workflow_diff(wf_diff, task_id=None):
+
+    duplicates = []
+    name, start = get_name_and_start(wf_diff.spec_diff)
+    task_info = f'({task_id})' if task_id is not None else ''
+
+    sys.stdout.write(f'\nResults for {name} {task_info}\n\n')
+    sys.stdout.write('Removed\n')
+    for task in wf_diff.removed:
+        if task.task_spec not in duplicates:
+            duplicates.append(task.task_spec)
+            label = f'{task.task_spec.bpmn_name or '-'} [{task.task_spec.name}]'
+            sys.stdout.write(f'{label:<50s} {TaskState.get_name(task.state)}\n')
+
+    sys.stdout.write('Changed\n')
+    for task in wf_diff.changed:
+        if task.task_spec not in duplicates:
+            duplicates.append(task.task_spec)
+            label = f'{task.task_spec.bpmn_name or '-'} [{task.task_spec.name}]'
+            sys.stdout.write(f'{label:<50s} {TaskState.get_name(task.state)}\n')
+
--- spiff_example/cli/subcommands.py
+++ spiff_example/cli/subcommands.py
@@ -2,6 +2,8 @@ import sys
 import json
 import logging
 
+from .diff_result import show_spec_diff, show_workflow_diff
+
 def configure_logging():
     spiff_logger = logging.getLogger('spiff')
     spiff_handler = logging.StreamHandler()
@@ -23,6 +25,10 @@ def show_library(engine, args):
     for spec_id, name, filename in engine.list_specs():
         sys.stdout.write(f'{spec_id}  {name:<20s} {filename}\n')
 
+def show_workflows(engine, args):
+    for wf_id, name, filename, active, started, updated in engine.list_workflows():
+        sys.stdout.write(f'{wf_id}  {name:<20s} {active} {started} {updated or ''}\n')
+
 def run(engine, args):
     instance = engine.start_workflow(args.spec_id)
     instance.run_until_user_input_required()
@@ -32,6 +38,25 @@ def run(engine, args):
     sys.stdout.write(json.dumps(instance.data, indent=2, separators=[', ', ': ']))
     sys.stdout.write('\n')
 
+def diff_spec(engine, args):
+    spec_diff = engine.diff_spec(args.original, args.new)
+    show_spec_diff(spec_diff)
+    if args.deps:
+        diffs, added = engine.diff_dependencies(args.original, args.new)
+        for diff in diffs.values():
+            show_spec_diff(diff)
+        if len(added) > 0:
+            sys.stdout.write('\nNew subprocesses\n')
+            sys.stdout.write('\n'.join(added))
+
+def diff_workflow(engine, args):
+    wf_diff, sp_diffs = engine.diff_workflow(args.wf_id, args.spec_id)
+    show_workflow_diff(wf_diff)
+    for task_id, result in sp_diffs.items():
+        show_workflow_diff(result, task_id=task_id)
+
+def migrate(engine, args):
+    engine.migrate_workflow(args.wf_id, args.spec_id, validate=not args.force)
 
 def add_subparsers(subparsers):
 
@@ -43,12 +68,32 @@ def add_subparsers(subparsers):
     add_spec.add_argument('-d', '--dmn', dest='dmn', nargs='*', metavar='FILE', help='DMN files to load')
     add_spec.set_defaults(func=add)
 
-    list_specs = subparsers.add_parser('list', help='List available specs')
+    list_specs = subparsers.add_parser('list_specs', help='List available specs')
     list_specs.set_defaults(func=show_library)
+
+    list_wf = subparsers.add_parser('list_instances', help='List instances')
+    list_wf.set_defaults(func=show_workflows)
 
     run_wf = subparsers.add_parser('run', help='Run a workflow')
     run_wf.add_argument('-s', '--spec-id', dest='spec_id', metavar='SPEC ID', help='The ID of the spec to run')
-    run_wf.add_argument('-a', '--active-ok', dest='active', action='store_true', help='Suppress exception if the workflow does not complete')
+    run_wf.add_argument('-a', '--active-ok', dest='active', action='store_true',
+                        help='Suppress exception if the workflow does not complete')
     run_wf.set_defaults(func=run)
 
+    compare_spec = subparsers.add_parser('diff_spec', help='Compare two workflow specs')
+    compare_spec.add_argument('-o', '--original', dest='original', metavar='SPEC ID', help='The ID of the original spec')
+    compare_spec.add_argument('-n', '--new', dest='new', metavar='SPEC ID', help='The ID of the new spec')
+    compare_spec.add_argument('-d', '--include-dependencies', action='store_true', dest='deps',
+                              help='Include dependencies in the output')
+    compare_spec.set_defaults(func=diff_spec)
 
+    compare_wf = subparsers.add_parser('diff_workflow', help='Compare a workflow against a new spec')
+    compare_wf.add_argument('-w', '--wf-id', dest='wf_id', metavar='WORKFLOW ID', help='The ID of the workflow')
+    compare_wf.add_argument('-s', '--spec-id', dest='spec_id', metavar='SPEC ID', help='The ID of the new spec')
+    compare_wf.set_defaults(func=diff_workflow)
+
+    migrate_wf = subparsers.add_parser('migrate', help='Update a workflow spec')
+    migrate_wf.add_argument('-w', '--wf-id', dest='wf_id', metavar='WORKFLOW ID', help='The ID of the workflow')
+    migrate_wf.add_argument('-s', '--spec-id', dest='spec_id', metavar='SPEC ID', help='The ID of the new spec')
+    migrate_wf.add_argument('-f', '--force', dest='force', action='store_true', help='Omit validation')
+    migrate_wf.set_defaults(func=migrate)
