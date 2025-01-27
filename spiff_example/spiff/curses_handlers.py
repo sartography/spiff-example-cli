c193d27238b71e0e7596190c0e71382f7549938c
Gunnar Pruß <gunnar.pruss@gfz-potsdam.de>
2025-01-27 14:45:34 +0100

    Rename the directory with the model definitions
    Rename the 'bpmn' directory into 'models' to i) prevent confusion with
    the identically named directory in the SpiffWorkflow library and ii)
    because it also contains DMN definitions.
    Only track changes in the 'camunda'- and 'tutorial'-subdirectory to
    allow the user to add their own workflows.

--- spiff_example/spiff/curses_handlers.py
+++ spiff_example/spiff/curses_handlers.py
@@ -6,7 +6,7 @@ from jinja2 import Template
 from ..curses_ui.user_input import SimpleField, Option, JsonField
 from ..curses_ui.human_task_handler import TaskHandler
 
-forms_dir = 'bpmn/tutorial/forms'
+forms_dir = 'models/tutorial/forms'
 
 class SpiffTaskHandler(TaskHandler):
 
